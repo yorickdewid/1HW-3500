47dc91ada3c72b8571e96d191dcaf77a9d7ffb62
Yorick de Wid <yorickdewid@users.noreply.github.com>
2014-10-20 20:26:13 +0200

    Create family.cpp

---
+++ family.cpp
@@ -0,0 +1,13 @@
+#include "family.h"
+
+//Default constructor
+Family::Family(){}
+
+//Default destructor
+Family::~Family(){}
+
+//Pre:	None
+//Post: Returns the private member familyNum as a string
+//Input:None
+string Family::getFamilyNum()
+{return familyNum;}
